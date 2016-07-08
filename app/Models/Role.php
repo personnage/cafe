f023cf723d3968158fa2b9966d03bf8e2a297dad
Lyubaev Kirill <lubaevka@gmail.com>
2016-07-08 17:06:36 +0300

    Добавлены scope для модели Role

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -23,6 +23,75 @@ class Role extends Model
      */
     protected $fillable = ['name', 'label'];
 
+    # Scopes
+
+    public function scopeOrderNameAsc($query)
+    {
+        return $query->orderBy('name', 'asc');
+    }
+
+    public function scopeOrderNameDesc($query)
+    {
+        return $query->orderBy('name', 'desc');
+    }
+
+    public function scopeOrderIdAsc($query)
+    {
+        return $query->orderBy('id', 'asc');
+    }
+
+    public function scopeOrderIdDesc($query)
+    {
+        return $query->orderBy('id', 'desc');
+    }
+
+    public function scopeOrderUpdatedAtAsc($query)
+    {
+        return $query->orderBy('updated_at', 'asc');
+    }
+
+    public function scopeOrderUpdatedAtDesc($query)
+    {
+        return $query->orderBy('updated_at', 'desc');
+    }
+
+    public function scopeFilter($query, $filterName)
+    {
+        switch ($filterName) {
+            case 'deleted':
+                return $query->onlyTrashed();
+            default:
+                return $query;
+        }
+    }
+
+    public function scopeSearch($query, $name)
+    {
+        $pattern = "%$name%";
+
+        return $query->where('name', 'ILIKE', $pattern)
+                  ->orWhere('label', 'ILIKE', $pattern)
+       ;
+    }
+
+    public function scopeSort($query, $sortName)
+    {
+        switch ($sortName) {
+            case 'name_asc':
+                return $query->orderNameAsc();
+            case 'id_desc':
+                return $query->orderIdDesc();
+            case 'id_asc':
+                return $query->orderIdAsc();
+            case 'updated_desc':
+                return $query->orderUpdatedAtDesc();
+            case 'updated_asc':
+                return $query->orderUpdatedAtAsc();
+            default:
+                return $query;
+        }
+    }
+
     /**
      * Get permissions to currently role.
      *
