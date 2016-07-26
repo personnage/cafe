52ab72da2c40c91d3b6b9796ead2a0fc9cf61265
Lyubaev Kirill <lubaevka@gmail.com>
2016-07-26 18:14:56 +0300

    Refactor events.

---
+++ app/Events/Confirmation/UserConfirmRegistration.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Events\Confirmation;
+
+use App\Events\Event;
+use App\Models\User;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class UserConfirmRegistration extends Event
+{
+    use SerializesModels;
+
+    /**
+     * @var User
+     */
+    public $user;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+}
