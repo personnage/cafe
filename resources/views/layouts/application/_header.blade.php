951593cc677d88cde512bbdebb84a076590e5998
Lyubaev Kirill <lubaevka@gmail.com>
2016-07-27 21:18:42 +0300

    Fix views.

--- resources/views/layouts/application/_header.blade.php
+++ resources/views/layouts/application/_header.blade.php
@@ -35,29 +35,7 @@
       <a href="{{ url("/login") }}" rel="nofollow">Вход</a>
     </div>
     @else
-    <div class="header__loggedBlock col-lg-4 col-md-4 hidden-xs">
-      <img src="http://assets.allcafe.ru/pic/avatars/40x40/0.jpeg" alt="" class="col-lg-3 col-md-3 pull-right">
-      <div class="info col-lg-3 col-md-3 col-sm-6 pull-right">
-        <a href="{{ url("community/feed") }}" class="info__profile">Мой AllCafe</a>
-        <span class="info__name">p.lebedev</span>
-        <a href="{{ url("community/out") }}" class="info__exit">выход</a>
-      </div>
-      <div class="icons col-lg-6 col-md-5 hidden-sm pull-right">
-        <a href="{{ url("community/feed#recommend") }}" class="icons__notification col-lg-3 col-md-4">
-          <span>3</span>
-        </a>
-        <a href="{{ url("community/feed#group") }}" class="icons__comment col-lg-3 col-md-4">
-          <span>10</span>
-        </a>
-        <a href="{{ url("community/users/inbox") }}" class="icons__mail col-lg-3 col-md-4">
-          <span>3</span>
-        </a>
-        <a href="{{ url("admin/errors") }}" class="icons__mail icons__admin col-md-3 visible-lg">
-          <span>444</span>
-        </a>
-      </div>
-    </div>
+      @include('layouts.application._profile')
     @endif
-
   </div>
 </div>
