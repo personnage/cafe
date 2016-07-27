49f0b7b53587bed97254c85eebcdf04e21557d2e
Lyubaev Kirill <lubaevka@gmail.com>
2016-07-27 17:32:16 +0300

    Fix Dashboard controller.

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,34 +4,55 @@ namespace App\Http\Controllers\Admin;
 
 use App\Models\Permission;
 use App\Models\Role;
-use App\Repositories\UserRepository;
+use App\Models\User;
 
 class DashboardController extends Controller
 {
     /**
-     * The user repository instance.
-     *
-     * @var UserRepository
+     * @return \Illuminate\Http\Response
      */
-    protected $users;
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct(UserRepository $users)
-    {
-        $this->users = $users;
-    }
-
     public function index()
     {
-        return view('admin.dashboard.index', [
-            'users' => $this->users->latest(),
-            'usersCount' => $this->users->totalUsers(),
-            'rolesCount' => Role::count(),
-            'permissionsCount' => Permission::count(),
-        ]);
+        $stats = $this->stats();
+
+        return view('admin.dashboard.index', compact('stats'));
+    }
+
+    /**
+     * Make stats object for rendering to dashboard.
+     *
+     * @return @anonymous
+     */
+    protected function stats()
+    {
+        return new class {
+            public function countUsers()
+            {
+                // or fetch from cache
+                return User::count();
+            }
+
+            public function countRoles()
+            {
+                return Role::count();
+            }
+
+            public function countPermissions()
+            {
+                return Permission::count();
+            }
+
+            public function latestUser()
+            {
+                return User::latest()->get();
+            }
+
+            public function latestNews()
+            {
+                //
+            }
+
+            //
+        };
     }
 }
