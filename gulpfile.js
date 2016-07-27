efb9182a511bc0c70d73888ae891ecdd48116d1d
Lyubaev Kirill <lubaevka@gmail.com>
2016-07-27 21:18:55 +0300

    Clean up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,93 +1,93 @@
-var elixir = require('laravel-elixir');
+const path = require('path');
+const elixir = require('laravel-elixir');
 require('laravel-elixir-stylus');
 
-elixir.config.sourcemaps = false;
+elixir.config.sourcemaps = true;
+
+var vendor_path = 'resources/assets/vendor/';
+var bower_components = path.join(vendor_path, 'bower_components');
 
 /**
  * Dashboard vendor dependencies.
  */
 elixir(function(mix) {
-    var vendor_path = 'resources/assets/vendor/';
-
     /**
      * Main js files without css dependencies.
      */
     mix.scripts([
-        'bower_components/jquery/dist/jquery.js',
+        'jquery/dist/jquery.js',
 
-    ], './public/assets/js/admin/lib.js', vendor_path);
+    ], './public/assets/js/admin/lib.js', bower_components);
 
     /**
      * JavaScript bootstrap set.
      */
     mix.scripts([
         // http://getbootstrap.com/getting-started/#support-ie10-width
-        'ie10-viewport-bug-workaround.js',
+        '../ie10-viewport-bug-workaround.js',
 
-        'bower_components/bootstrap/dist/js/bootstrap.js',
+        'bootstrap/dist/js/bootstrap.js',
 
-        'bower_components/bootstrap-material-design/dist/js/material.js',
-        'bower_components/bootstrap-material-design/dist/js/ripples.js',
+        'bootstrap-material-design/dist/js/material.js',
+        'bootstrap-material-design/dist/js/ripples.js',
 
-        'bower_components/remarkable-bootstrap-notify/dist/bootstrap-notify.js',
+        'remarkable-bootstrap-notify/dist/bootstrap-notify.js',
 
-    ], './public/assets/js/admin/bootstrap-pkg.js', vendor_path);
+    ], './public/assets/js/admin/bootstrap-pkg.js', bower_components);
 
     mix.scripts([
-        'bower_components/selectize/dist/js/standalone/selectize.js',
+        'selectize/dist/js/standalone/selectize.js',
 
-    ], './public/assets/js/admin/selectize.js', vendor_path);
+    ], './public/assets/js/admin/selectize.js', bower_components);
 
     /**
      * CSS bootstrap set.
      */
     mix.styles([
         // http://getbootstrap.com/getting-started/#support-ie10-width
-        'ie10-viewport-bug-workaround.css',
+        '../ie10-viewport-bug-workaround.css',
 
-        'bower_components/bootstrap/dist/css/bootstrap.css',
+        'bootstrap/dist/css/bootstrap.css',
 
-        'bower_components/bootstrap-material-design/dist/css/bootstrap-material-design.css',
-        'bower_components/bootstrap-material-design/dist/css/ripples.css',
+        'bootstrap-material-design/dist/css/bootstrap-material-design.css',
+        'bootstrap-material-design/dist/css/ripples.css',
 
-    ], './public/assets/css/admin/bootstrap-pkg.css', vendor_path);
+    ], './public/assets/css/admin/bootstrap-pkg.css', bower_components);
 });
 
 /**
  * Application vendor dependencies.
  */
 elixir(function(mix) {
-    var vendor_path = 'resources/assets/vendor/';
-
     /**
      * Main js files without css dependencies.
      */
     mix.scripts([
-        'bower_components/jquery/dist/jquery.js',
+        'jquery/dist/jquery.js',
 
-    ], './public/assets/js/app/lib.js', vendor_path);
+    ], './public/assets/js/app/lib.js', bower_components);
 
     /**
      * JavaScript bootstrap set.
      */
     mix.scripts([
         // http://getbootstrap.com/getting-started/#support-ie10-width
-        'ie10-viewport-bug-workaround.js',
+        '../ie10-viewport-bug-workaround.js',
 
-        'bower_components/bootstrap/dist/js/bootstrap.js',
+        'bootstrap/dist/js/bootstrap.js',
 
-    ], './public/assets/js/app/bootstrap-pkg.js', vendor_path);
+    ], './public/assets/js/app/bootstrap-pkg.js', bower_components);
 
     /**
      * CSS bootstrap set.
      */
     mix.styles([
         // http://getbootstrap.com/getting-started/#support-ie10-width
-        'ie10-viewport-bug-workaround.css',
+        '../ie10-viewport-bug-workaround.css',
 
-        'bower_components/bootstrap/dist/css/bootstrap.css',
+        'bootstrap/dist/css/bootstrap.css',
 
-    ], './public/assets/css/app/bootstrap-pkg.css', vendor_path);
+    ], './public/assets/css/app/bootstrap-pkg.css', bower_components);
 });
 
 /**
