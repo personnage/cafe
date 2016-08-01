c0daad1185672a1ac8966d83d4f4a33fbfa8e59e
Lyubaev Kirill <lubaevka@gmail.com>
2016-08-01 20:42:19 +0300

    Add new scope trait NewsCategory.
    - add new relation: parent/childs

--- app/Models/NewsCategory.php
+++ app/Models/NewsCategory.php
@@ -6,7 +6,21 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class NewsCategory extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, Scopes\NewsCategory;
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'title', 'description'];
 
     public function image()
     {
@@ -16,5 +30,15 @@ class NewsCategory extends Model
     public function items()
     {
         return $this->hasMany(NewsItem::class);
+    }
+
+    public function parent()
+    {
+        return $this->hasOne(static::class, 'id', 'parent_id');
+    }
+
+    public function childs()
+    {
+        return $this->hasMany(static::class, 'parent_id', 'id');
     }
 }
