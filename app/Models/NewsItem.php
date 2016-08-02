c6e454ee911ec72f401f6b648f27fa1c7e4c05ef
Lyubaev Kirill <lubaevka@gmail.com>
2016-08-02 15:28:46 +0300

    Add soft delete news item model.

--- app/Models/NewsItem.php
+++ app/Models/NewsItem.php
@@ -2,8 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 class NewsItem extends Model
 {
+    use SoftDeletes;
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
+
     public function category()
     {
         return $this->belongsTo(NewsCategory::class);
