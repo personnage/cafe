56b2e89982730353d5b41b8155b9ae0c22a9823e
Lyubaev Kirill <lubaevka@gmail.com>
2016-08-09 16:16:25 +0300

    Remove NewsCategoryImage model. Update NewsCategory model.

--- app/Models/NewsCategory.php
+++ app/Models/NewsCategory.php
@@ -2,11 +2,18 @@
 
 namespace App\Models;
 
+use Storage;
+use App\Jobs\ReleaseNewsCategory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class NewsCategory extends Model
 {
     use SoftDeletes, Scopes\NewsCategory;
+
+    /**
+     * @var string Base path to saving thumbnail.
+     */
+    protected $thumbnailBasePath = 'thumbnails/news/category';
 
     /**
      * The attributes that should be mutated to dates.
@@ -20,11 +27,34 @@ class NewsCategory extends Model
      *
      * @var array
      */
-    protected $fillable = ['name', 'title', 'description'];
+    protected $fillable = ['title', 'description', 'thumbnail'];
 
-    public function image()
+    /**
+     * Set a URL friendly "slug" from the given name.
+     *
+     * This is method idempotence.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    public function setNameAttribute($value)
     {
-        return $this->hasOne(NewsCategoryImage::class);
+        $this->attributes['name'] = str_slug($value);
+    }
+
+    /**
+     * Get thumbnail name.
+     *
+     * @param  string  $value
+     * @return string|null
+     */
+    public function getThumbnailAttribute($value)
+    {
+        if (! is_null($value)) {
+            return $this->thumbnailBasePath.DIRECTORY_SEPARATOR.$value;
+        }
+
+        return $value;
     }
 
     public function items()
@@ -40,5 +70,41 @@ class NewsCategory extends Model
     public function childs()
     {
         return $this->hasMany(static::class, 'parent_id', 'id');
+    }
+
+    /**
+     * Get full path to thumbnail storage.
+     *
+     * @param  string  $filename
+     * @return string
+     */
+    public function thumbnailUrl()
+    {
+        return Storage::url($this->thumbnail);
+    }
+
+    /**
+     * Upload thumbnail file to given category.
+     *
+     * @param  string|resource  $contents]
+     * @return bool
+     */
+    public function uploadThumbnail($contents)
+    {
+        $that = clone $this;
+        $this->thumbnail = str_random();
+
+        $result = Storage::disk('public')->put($this->thumbnail, $contents);
+
+        if (! $result) {
+            // restore thumbnail
+            $this->thumbnail = $that->thumbnail;
+
+            return $result;
+        }
+
+        dispatch(new ReleaseNewsCategory($that));
+
+        return true;
     }
 }
