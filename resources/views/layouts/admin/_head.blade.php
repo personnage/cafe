71a5f020e13d6b1fbf9d4d318d167281147d0bf7
Lyubaev Kirill <lubaevka@gmail.com>
2016-08-09 16:04:16 +0300

    Remove Material Icons.

--- resources/views/layouts/admin/_head.blade.php
+++ resources/views/layouts/admin/_head.blade.php
@@ -15,9 +15,7 @@
 
   @stack('meta-tags')
 
-  <link href='https://fonts.googleapis.com/css?family=Scada:400,700,400italic&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
+  <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Scada:400,400italic,700' type='text/css'>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.css">
 
   <link rel="stylesheet" href="{{ elixir('assets/css/admin/bootstrap-pkg.css') }}">
