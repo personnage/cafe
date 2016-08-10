f2812b8b8cb4e8f8cff7965076a76dd7f84b6a08
Lyubaev Kirill <lubaevka@gmail.com>
2016-08-10 16:17:22 +0300

    Fix order_by acs -> asc.

--- app/Models/Scopes/NewsItem.php
+++ app/Models/Scopes/NewsItem.php
@@ -55,7 +55,7 @@ trait NewsItem
             case 'id_desc':
                 return $query->orderBy('id', 'desc');
             case 'id_asc':
-                return $query->orderBy('id', 'acs');
+                return $query->orderBy('id', 'asc');
             case 'updated_desc':
                 return $query->orderBy('updated_at', 'desc');
             case 'updated_asc':
--- app/Models/Scopes/User.php
+++ app/Models/Scopes/User.php
@@ -81,7 +81,7 @@ trait User
             case 'id_desc':
                 return $query->orderBy('id', 'desc');
             case 'id_asc':
-                return $query->orderBy('id', 'acs');
+                return $query->orderBy('id', 'asc');
             case 'updated_desc':
                 return $query->orderBy('updated_at', 'desc');
             case 'updated_asc':
